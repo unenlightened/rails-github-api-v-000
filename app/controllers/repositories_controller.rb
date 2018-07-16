488f116ac7d00963f01dc33bd66511ced276247c
Chely Ho <adept_inept@yahoo.com>
2018-07-16 23:09:48 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,7 +7,7 @@ class RepositoriesController < ApplicationController
 
     resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     @repos = JSON.parse(resp.body)
-    
+
     @username = session[:username]
   end
 
